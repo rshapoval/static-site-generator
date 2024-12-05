5f38d89fb8713ec0895e70382e4f103b2e6e9075
Ruslan Shapoval <shapoval.websites@gmail.com>
2024-12-05 17:37:06 +0200

    feat: add hero section data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+import { initialState, PageData } from "../interfaces/page";
 import Breadcrumbs from "@/components/dashboard/breadcrumbs";
 import Buttons from "@/components/dashboard/buttons";
 import HeroSection from "@/components/dashboard/landing-sections/hero-section";
@@ -7,6 +9,8 @@ import HeroSection from "@/components/dashboard/landing-sections/hero-section";
 const BREADCRUMBS_LINKS = [{ text: "Dashboard" }];
 
 export default function Dashboard() {
+  const [data, setData] = useState<PageData>(initialState);
+
   return (
     <>
       <div className="flex flex-wrap">
@@ -15,7 +19,7 @@ export default function Dashboard() {
       </div>
       <hr />
       <div className="text-lg">
-        <HeroSection />
+        <HeroSection data={data} setData={setData} />
       </div>
     </>
   );
--- components/dashboard/landing-sections/hero-section.tsx
+++ components/dashboard/landing-sections/hero-section.tsx
@@ -1,30 +1,53 @@
+import { HeroData, PageData } from "@/app/interfaces/page";
 import TextField from "../text-field";
+import { ChangeEvent } from "react";
 
-export default function HeroSection() {
+interface HeroSectionProps {
+  data: PageData;
+  setData: (newData: PageData) => void;
+}
+
+export default function HeroSection({ data, setData }: HeroSectionProps) {
+  const handleChange =
+    (field: keyof HeroData) => (event: ChangeEvent<HTMLInputElement>) => {
+      setData({
+        ...data,
+        hero: {
+          ...data.hero,
+          [field]: event.target.value,
+        },
+      });
+    };
+
   return (
     <div className="mt-4">
-      <TextField label="H1" id="h1" value="" handleChange={(event) => {}} />
+      <TextField
+        label="H1"
+        id="h1"
+        value={data["hero"]["h1"]}
+        handleChange={handleChange("h1")}
+      />
       <TextField
         label="Subtitle"
         id="subtitle"
-        value=""
-        handleChange={(event) => {}}
+        value={data["hero"]["subtitle"]}
+        handleChange={handleChange("subtitle")}
       />
       <div className="flex">
         <div className="shrink-0 w-1/2 pr-2">
           <TextField
             label="Primary link text"
             id="primary-link-text"
-            value=""
-            handleChange={(event) => {}}
+            value={data["hero"]["primary-link-text"]}
+            handleChange={handleChange("primary-link-text")}
           />
         </div>
         <div className="shrink-0 w-1/2 pl-2">
           <TextField
             label="Primary link url"
             id="primary-link-url"
-            value=""
-            handleChange={(event) => {}}
+            value={data["hero"]["primary-link-url"]}
+            handleChange={handleChange("primary-link-url")}
           />
         </div>
       </div>
@@ -33,16 +56,16 @@ export default function HeroSection() {
           <TextField
             label="Secondary link text"
             id="secondary-link-text"
-            value=""
-            handleChange={(event) => {}}
+            value={data["hero"]["secondary-link-text"]}
+            handleChange={handleChange("secondary-link-text")}
           />
         </div>
         <div className="shrink-0 w-1/2 pl-2">
           <TextField
             label="Secondary link url"
             id="secondary-link-url"
-            value=""
-            handleChange={(event) => {}}
+            value={data["hero"]["secondary-link-url"]}
+            handleChange={handleChange("secondary-link-url")}
           />
         </div>
       </div>
