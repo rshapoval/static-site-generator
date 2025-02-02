6ca6a2ce87d17295040c10436eff43429b3b9392
Ruslan Shapoval <shapoval.websites@gmail.com>
2025-02-02 19:07:16 +0200

    feat: add export css

--- app/api/export/route.ts
+++ app/api/export/route.ts
@@ -1,39 +1,55 @@
-import JSZip from "jszip";
 import { NextResponse } from "next/server";
+import { exec } from "child_process";
+import { promisify } from "util";
+import JSZip from "jszip";
+import fs from "fs";
+import path from "path";
+
+const execPromise = promisify(exec);
 
 export const POST = async () => {
   try {
+    const globalsCSSPath = path.join(process.cwd(), "app", "globals.css");
+    const outputCSSPath = path.join(process.cwd(), "public", "tmp-styles.css");
+
+    await execPromise(
+      `npx tailwindcss -i ${globalsCSSPath} -o ${outputCSSPath}`,
+    );
+
+    const compiledCSS = fs.readFileSync(outputCSSPath, "utf8");
+
     const zip = new JSZip();
 
     const htmlContent = `
       <!DOCTYPE html>
       <html lang="en">
-        <head>
+      <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Exported Page</title>
         <link rel="stylesheet" href="styles.css">
       </head>
       <body>
-        <h1>Hello, world!</h1>
+        <h1 class="button">Hello, world!</h1>
       </body>
-      </html>
-    `;
-
-    const cssContent = `body { font-family: Arial, sans-serif; background: #f4f4f4; text-align: center; }`;
+      </html>`;
 
     zip.file("index.html", htmlContent);
-    zip.file("styles.css", cssContent);
+    zip.file("styles.css", compiledCSS);
 
     const zipContent = await zip.generateAsync({ type: "nodebuffer" });
 
+    fs.unlinkSync(outputCSSPath);
+    fs.unlinkSync(outputCSSPath + ".map");
+
     const headers = new Headers({
-      "Content-Disposition": "attachment; filename=static-site.zip",
+      "Content-Disposition": "attachment; filename=export.zip",
       "Content-Type": "application/zip",
     });
 
     return new NextResponse(zipContent, { status: 200, headers });
   } catch (error) {
+    console.error("Export error:", error);
     return new NextResponse(
       JSON.stringify({ error: "Internal Server Error" }),
       { status: 500 },
